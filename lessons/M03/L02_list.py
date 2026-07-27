7f87285aa495ac78a58a6b2eb69df2c7ca7bb81b
TU <n4ypk5dm85@privaterelay.appleid.com>
2026-07-27 16:44:37 +0700

    M3-2 完成 List

---
+++ lessons/M03/L02_list.py
@@ -0,0 +1,24 @@
+# ETF清單
+
+etf_list = [
+    "00918",
+    "00919",
+    "00713",
+    "0056",
+    "00982A"
+]
+
+print(etf_list)
+etf_list.append("00915")
+print(etf_list)
+print(len(etf_list))
+print(etf_list[0])
+etf_list[3] = "00929"
+print(etf_list)
+favorite_etf = [
+    "00918",
+    "00919",
+    "00713"
+]
+favorite_etf.append("00982A")
+print("我的ETF清單:", favorite_etf)
--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 print("welcome to use TW ETF AI Analyzer")
-etf_code = "00918"
-etf_name = "大華優利高填息30"
-price = 22.56
-dividend = 0.84
-print("ETF代號：", etf_code)
-print("ETF名稱：", etf_name)
-print("目前價格：", price)
-print("最近配息：", dividend)
-company = "大華投信"
-fund_size = 380
-expense_ratio = 0.30
-is_monthly = True
