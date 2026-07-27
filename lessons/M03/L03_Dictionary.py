8af8289247be60937a459e1109eb688cc0758b13
TU <n4ypk5dm85@privaterelay.appleid.com>
2026-07-27 22:18:37 +0700

    M3-3 完成 Dictionary

---
+++ lessons/M03/L03_Dictionary.py
@@ -0,0 +1,41 @@
+etf = {
+    "code": "00918",
+    "name": "大華優利高填息30",
+    "price": 22.56,
+    "dividend": 0.84,
+    "monthly": True
+}
+
+print(etf)
+print(etf["code"])
+print(etf["name"])
+etf["expense_ratio"] = 0.30
+print(etf)
+etf["price"] = 22.88
+print(etf["price"])
+my_etf = {
+    "code": "00919",
+    "name": "群益台灣精選高息",
+    "price": 23.45,
+    "dividend": 0.72,
+    "monthly": True
+}
+
+print("ETF代號：", my_etf["code"])
+print("ETF名稱：", my_etf["name"])
+print("價格：", my_etf["price"])
+print("配息：", my_etf["dividend"])
+etf_database = [
+    {
+        "code": "00918",
+        "name": "大華優利高填息30",
+        "price": 22.56
+    },
+    {
+        "code": "00919",
+        "name": "群益台灣精選高息",
+        "price": 23.45
+    }
+]
+
+print(etf_database)
